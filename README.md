dc8c2ae88d415bd5e55d40e43dbb0e484d304dd9
zbianca <bianca.zanardi@gmail.com>
2018-04-15 23:54:01 +0200

    add all basic requirements

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-frontend-nanodegree-arcade-game
-===============================
+# Arcade Game Project
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+This project is part of the Front-End Web Developer Nanodegree Program by [Udacity](https://www.udacity.com/).
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+> [Play the game!](https://zbianca.github.io/frogger-game/index.html)
+
+## Requirements
+
+* Player cannot move off screen
+* Vehicles cross the screen
+* Vehicle-player collisions happen logically (not too early or too late)
+* Vehicle-player collision resets the game
+* Something happens when player wins
+* Game objects (player and vehicles) are implemented using JavaScript object-oriented programming features.
+
+## Resources
+
+* [Udacity's original repository](https://github.com/udacity/frontend-nanodegree-arcade-game)
+* [Udacity Frontend Nanodegree Style Guide](http://udacity.github.io/frontend-nanodegree-styleguide/javascript.html)
--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+// Function to restrain a number (nr) to a range
+function limit(min, max, nr) {
+    const nr2 = Math.max(min, nr);
+    return Math.min(nr2, max);
+}
+
+// Returns a random number between min (included) and max (excluded)
+function randomBetween(min, max) {
+    return Math.floor(min + Math.random() * (max - min))
+}
+
 class Entity {
     constructor(x, y, speed, sprite) {
         // Set initial location
@@ -8,7 +19,7 @@ class Entity {
         this.sprite = sprite;
     }
 
-    // Draw the enemy on the screen, required method for game
+    // Draw the entities on the screen, required method for game
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
@@ -16,58 +27,88 @@ class Entity {
 
 // Enemies our player must avoid
 class Enemy extends Entity {
-    constructor(x, y, speed) {
+    constructor(x, y, speed, player) {
         super(x, y, speed, 'images/enemy-bug.png');
-        // The image/sprite for our enemies, this uses
-        // a helper we've provided to easily load images
+        this.player = player;
     }
 
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
     update(dt) {
-        // You should multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for
+        // Multiply any movement by the dt parameter
+        // to ensure the game runs at the same speed for
         // all computers.
         this.x += this.speed * dt;
+        this.checkCollisions();
     }
+
+    checkCollisions() {
+        let sameRow = false;
+        let sameCol = false;
+        //[62, 146, 228] // Enemy lanes, axis y
+        //[60, 140, 220] // Player lanes, axis y
+        if ((this.y === 62 && this.player.y === 60) || (this.y === 146 && this.player.y === 140) || (this.y === 228 && this.player.y === 220)) {
+            sameRow = true;
+        }
+        //[0, 101, 202, 303, 404] // Player current x values in each tile
+        // Add 75 pixels margin to each side // Enemy width
+        if (this.player.x + 75 > this.x && this.player.x - 75 < this.x) {
+            sameCol = true;
+        }
+        if (sameRow === true && sameCol === true) {
+            player.die();
+        }
+    }
+
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-class Player extends Entity  {
+class Player extends Entity {
     constructor(x, y, speedX, speedY) {
         super(x, y, speedX, 'images/char-cat-girl.png');
+        // Player has one velocity for each direction
+        // because the tiles are rectangular.
         this.speedX = speedX;
         this.speedY = speedY;
-        this.moveX = 0;
-        this.moveY = 0;
+        this.moveX = 0; // Multiplier to indicate direction of horizontal movements
+        this.moveY = 0; // Multiplier to indicate direction of horizontal movements
+        this.beginX = x; // Save initial coordinates for resetting
+        this.beginY = y;
+        // this.lives = 3; TODO: Life counter
+        this.gameOver = false;
     }
 
-    // Update the enemy's position, required method for game
+    // Update the player's position, required method for game
     // Parameter: dt, a time delta between ticks
     update(dt) {
-        // You should multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for
+
+        // Stop update while resetting game
+        if (this.gameOver) {
+            return;
+        }
+
+        // Multiply any movement by the dt parameter
+        // to ensure the game runs at the same speed for
         // all computers.
         dt = dt || 1;
         if (this.moveX !== 0) {
             this.x += this.speedX * dt * this.moveX;
-            this.x = Player.limit(0, 404, this.x);
+            this.x = limit(0, 404, this.x); // Restrain movement within canvas
         }
         if (this.moveY !== 0) {
             this.y += this.speedY * dt * this.moveY;
-            this.y = Player.limit(-20, 380, this.y);
+            this.y = limit(-20, 380, this.y); // Restrain movement within canvas
         }
         this.moveX = 0;
         this.moveY = 0;
+
+        if (this.y === -20) { // Position y when reaching the water
+            this.gameOver = true;
+            // Timeout allows player to see their last movement
+            window.setTimeout(this.victory.bind(this), 300);
+        }
     }
 
-    static limit(min, max, nr) {
-        let nr2 = Math.max(min, nr);
-        return Math.min(nr2, max);
-    }
-
+    // Save direction of movement into a multiplier
     handleInput(dir) {
         switch (dir) {
             case 'left':
@@ -84,27 +125,42 @@ class Player extends Entity  {
                 break;
         }
     }
+
+    die() {
+        this.x = this.beginX;
+        this.y = this.beginY;
+        // TODO: implement life counter
+        // if (this.lives > 1) {
+        //     this.lives -= 1;
+        // } else {
+        window.alert("Try again!");
+        // }
+    }
+
+    victory() {
+        window.alert("You win!");
+        this.x = this.beginX;
+        this.y = this.beginY;
+        this.gameOver = false;
+    }
+
 }
 
-// Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 const allEnemies = [];
 const player = new Player(202, 380, 101, 80);
 
+// Instantiate random new enemies every 3 seconds
+setInterval(function createEnemies() {
+    const enemyYs = [62, 146, 228]; // Enemy lanes
+    const randomY = enemyYs[randomBetween(0, 3)];
+    const randomSpeed = randomBetween(50, 65);
+    const enemy = new Enemy(-80, randomY, randomSpeed, player);
+    allEnemies.push(enemy);
+}, 3000);
 
-const enemy = new Enemy(0, 62, 50);
-const enemy1 = new Enemy(0, 146, 80);
-const enemy2 = new Enemy(0, 228, 30);
-// lane1 = 62;
-// lane2 = 146;
-// lane3 = 228;
-allEnemies.push(enemy);
-allEnemies.push(enemy1);
-allEnemies.push(enemy2);
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys
+// to Player.handleInput() method.
 document.addEventListener('keyup', e => {
     const allowedKeys = {
         37: 'left',
